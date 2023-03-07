90f66bd422aa0d8ba3aa8a048f1f525bd3a1b9ae
Michele Sessa <mikysett@gmail.com>
2023-03-07 00:33:25 +0000

    content: add info in programming
    fix minor content issues in bio

--- cv-frontend/src/views/BioView.vue
+++ cv-frontend/src/views/BioView.vue
@@ -4,38 +4,28 @@
 
     <Separator />
 
-    <div class="long-description">
-      I've started programming when I was fourteen. First I've played with HTML and CSS and then I've bought an amazing
-      manual for learning C, this is how I've discovered one of the most durable and fulfilling passions of my life (so
-      far).
-      <br><br>
-      I've seen programming as an empowering tool to make things happen, but I was scared about making it my job, thinking
-      I was not good enough.
-      <br><br>
-      During university (oriental cultures and languages) I've made multiple websites (with an handmade back-end in PHP)
-      for couple of associations I was active in. It was an amateur theater and an environmental association. By the way,
-      I deeply care about nature and I love performative art.
-      <br><br>
-      Growing up I've ended up working a lot in Hotels, from reception clerk to hotel manager. As an hotel manager I've
-      decided to create a booking managing system since we missed one and as a receptionist on the only hotel of a private
-      island I've made a guest transfer system to better coordinate boats, helicopters (oh yeah, people were coming with
-      an helicopter to our hotel, I even had the chance to enjoy a free ride once) and taxis pick-ups for our guests.
-      <br><br>
-      In both cases our staff and guests greatly enjoyed the benefices of those applications and I experienced one more
-      time how useful programming could be when applied to real-life problems.
-      <br><br>
-      Then life continue, I like to travel, I find myself in Lisbon, working in the SEA marketing industry.
-      <br><br>
-      During pandemic I discover a 42 School is opening nearby. I decide to try my chances, mainly not to have any regrets
-      but feeling once again I won't be good enough to pass the one month intensive admission tests.
-      <br><br>
-      At my greatest surprise not only am I accepted into the school but I'm also the one that passed the tests with the
-      greatest score in the entire school history in Lisbon (which now includes thousands of candidates).
-      <br><br>
-      From there on my goal become clearer and more linear and my experiences start to fit better into a standard CV.
-      <br><br>
-      I'm still a bit scared I'm not good enough, but I know now I'm definitely happy to try my best 😊
-    </div>
+    <div class="long-description">I've started programming when I was fourteen. First I've played with HTML and CSS and
+      then I've bought an amazing manual for learning C, this is how I've discovered one of the most durable and
+      fulfilling passions of my life (so far).<br><br>I've seen programming as an empowering tool to make things happen,
+      but I was scared about making it my job, thinking I was not good enough.<br><br>During university (oriental cultures
+      and languages) I've made multiple websites (with an handmade back-end in PHP) for couple of associations I was
+      active in. It was an amateur theater and an environmental association. By the way, I deeply care about nature and I
+      love performative art.<br><br>Growing up I've ended up working a lot in Hotels, from reception clerk to hotel
+      manager. As an hotel manager I've decided to create a <a href="https://github.com/mikysett/horrible_booking_system"
+        target="_blank" class="text-link">booking managing system</a> since we missed one and as a receptionist on the
+      only hotel of a private island I've made a <a href="https://github.com/mikysett/horrible_guest_transfer_system"
+        target="_blank" class="text-link">guest transfer system</a> to better coordinate boats, helicopters (oh yeah,
+      people were coming with an helicopter to our hotel, I even had the chance to enjoy a free ride once) and taxis
+      pick-ups for our guests.<br><br>In both cases our staff and guests greatly enjoyed the benefices of those
+      applications and I experienced one more time how useful programming could be when applied to real-life
+      problems.<br><br>Then life continue, I like to travel, I find myself in Lisbon, working in the SEA marketing
+      industry.<br><br>During pandemic I discover a <a href="https://www.42network.org/" target="_blank"
+        class="text-link">42 School</a> is opening nearby. I decide to try my chances, mainly not to have any regrets but
+      feeling once again I won't be good enough to pass the one month intensive admission tests.<br><br>At my greatest
+      surprise not only am I accepted into the school but I'm also the one that passed the tests with the greatest score
+      in the entire school history in Lisbon (which now includes thousands of candidates).<br><br>From there on my goal
+      become clearer and more linear and my experiences start to fit better into a standard CV.<br><br>I'm still a bit
+      scared I'm not good enough, but I know now I'm definitely happy to try my best 😊</div>
 
     <Separator />
 
@@ -45,15 +35,19 @@
         <ul role="list" class="list">
           <li class="list-item">🧗 Outdoor climbing! (best way to spend a week-end)</li>
           <li class="list-item">📚 Reading a nice book (currently "L'innommable" from Beckett)</li>
-          <li class="list-item">🍆 Cooking traditional Italian dishes (my favorite is <a href="https://ricette.giallozafferano.it/Parmigiana-di-melanzane.html" target="_blank" class="text-link">Parmigiana</a>)</li>
+          <li class="list-item">🍆 Cooking traditional Italian dishes (my favorite is <a
+              href="https://ricette.giallozafferano.it/Parmigiana-di-melanzane.html" target="_blank"
+              class="text-link">Parmigiana</a>)</li>
           <li class="list-item">🌈 Helping out in an association that delivers hot meals to families in need<br>
             (actually I feel they are helping me more than I'm helping them)</li>
           <li class="list-item">💃 Dancing and having fun with friends</li>
-      </ul>
+        </ul>
+      </div>
     </div>
+
   </div>
 
-</div></template>
+</template>
 
 <script>
 import Separator from '@/components/Separator.vue'
--- cv-frontend/src/views/ProgrammingView.vue
+++ cv-frontend/src/views/ProgrammingView.vue
@@ -10,6 +10,41 @@
 
     <Separator />
 
+    <code class="prog-description">$ cat **/* | head -n 10 &amp;&amp; echo "10$?% open to speak more about it"<br>
+        # C (nice), Rust (I love it), C++ (... nothing to declare)<br>
+        # A bit of: Lisp, JS, Python, PHP, Go,(HTML/CSS... does it count?)<br>
+        # Emacs keybindings (swapping caps lock and ctrl)<br>
+        # Let's make an interactive rebase to squash those atomic commits<br>
+        # Test Driven Development (and save years of my life)<br>
+        # You have Windows?! Ok, I send you a Dockerfile ;)<br>
+        # I've made a swagger for the API, check the entrypoints<br>
+        # I have to do it more than twice? Here is a bash script<br>
+        # O(1) is my goal, O(n) will be fine (please not O(n!) again!)<br>
+        # Like agile, sprints, atomic tasks? me too :)<br>
+        100% open to speak more about it<br>
+        $ </code>
+
+    <Separator />
+
+    <div class="extra-info-wrapper">
+      <h2 class="role-title ei-title">When I'm not coding</h2>
+      <div class="ei-container no-p">
+        <ul role="list" class="list">
+          <li class="list-item">- <a href="https://en.wikipedia.org/wiki/Hackers_%26_Painters" target="_blank"
+              class="text-link">Hackers &amp; Painters</a> (started my interest for functional programming)</li>
+          <li class="list-item">- <a href="https://mitpress.mit.edu/9780262539807/the-elements-of-computing-systems/"
+              target="_blank" class="text-link">The Elements of Computing Systems</a> (mind blowing)</li>
+          <li class="list-item">- <a href="https://www.oreilly.com/library/view/exercises-in-programming/9781482227376/"
+              target="_blank" class="text-link">Exercises in programming style</a> (very fun to do)</li>
+          <li class="list-item">- Doing my own Bash (Aka <a href="https://github.com/mikysett/minishell" target="_blank"
+              class="text-link">minishell</a><a href="https://ricette.giallozafferano.it/Parmigiana-di-melanzane.html"
+              target="_blank" class="text-link"></a>)</li>
+          <li class="list-item">- Starting to contribute to <a href="https://github.com/rust-lang/rust" target="_blank"
+              class="text-link">rustc</a> repo (no, I didn't make any RFC, I'm not a genius)</li>
+        </ul>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -27,4 +62,16 @@ export default {
 </script>
 
 <style scoped>
+code {
+  overflow: auto;
+  padding: 12px 8px;
+  display: block;
+  flex: 1;
+  background: #300a24;
+  color: #fff4ea;
+  font-family: "Cutive Mono", monospace;
+  font-size: 14px;
+  line-height: 22px;
+  white-space: nowrap;
+}
 </style>
